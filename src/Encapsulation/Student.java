dd1c78f2593d424e1f6463a9b010482d975e887b
nilufar-easmin <enilufar1985@gmail.com>
2023-08-04 19:26:32 +0600

    first commit

---
+++ src/Encapsulation/Student.java
@@ -0,0 +1,11 @@
+package Encapsulation;
+
+public class Student {
+    String name;
+    int id;
+
+    void display(){
+        System.out.println("Name:"+name);
+        System.out.println("ID"+id);
+    }
+}
---
+++ src/Encapsulation/Test.java
@@ -0,0 +1,10 @@
+package Encapsulation;
+
+public class Test {
+    public static void main(String[] args) {
+        Student s1=new Student();
+        s1.name="Nilu";
+        s1.id=11;
+    s1.display();
+    }
+}
